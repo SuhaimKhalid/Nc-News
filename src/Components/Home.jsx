7727c5d5f8c2740abcbb069981ead817d6f2dcf2
Suhaim <suhaimkhalid007@gmail.com>
2025-05-19 16:56:53 +0100

    Trying to do css in Tailwind

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,19 @@
+import { HomeCoding } from "./HomeComponents/HomeCoding";
+
 export const Home = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello Home!</h1>
+      <section className="home_section">
+        <h2>Nc News</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nobis
+          sunt possimus quos odio. Vitae, accusamus possimus? Optio obcaecati
+          voluptatum hic officiis nam maiores, modi ea laboriosam tenetur
+          dolores suscipit sapiente similique quam quis ad odio vel dolore quas
+          fugiat.
+        </p>
+      </section>
+      <HomeCoding />
     </>
   );
 };
---
+++ src/Components/HomeComponents/HomeCoding.jsx
@@ -0,0 +1,22 @@
+export const HomeCoding = () => {
+  return (
+    <>
+      <section className="home-article">
+        <h3 className="text-center text-3xl font-bold">Coding</h3>
+        <article className="row">
+          <div className="left-side-home-article">
+            <img src="" alt="Coding Image" />
+          </div>
+          <div className="right-side-home-article">
+            <h3>Article Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              repellendus praesentium officia quam consequuntur quidem
+              reprehenderit? Eveniet deleniti libero facere.
+            </p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+};
