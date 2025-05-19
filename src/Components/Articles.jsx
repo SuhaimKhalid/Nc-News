29ebe604fa40792812c0a4a37ca3b5f17e0c2b18
Suhaim <suhaimkhalid007@gmail.com>
2025-05-19 15:42:44 +0100

    Make A working Navbarwith React Router

---
+++ src/Components/Articles.jsx
@@ -0,0 +1,7 @@
+export const Articles = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Articles!</h1>
+    </>
+  );
+};
---
+++ src/Components/Comments.jsx
@@ -0,0 +1,7 @@
+export const Comments = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Comments!</h1>
+    </>
+  );
+};
